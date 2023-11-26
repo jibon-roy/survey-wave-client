b31bad360764950e7a9cd62810b445e7ab2ef4ee
jibon-roy <mailme.jibon@gmail.com>
2023-11-26 15:37:05 +0600

    Authcontext and auth provider added in project

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './utility/AuthProvider/AuthProvider'
 
 const queryClient = new QueryClient()
 
@@ -16,13 +17,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CustomThemeProvider>
-        <div className='text-[#0300303] bg-[#F5F5F5]'>
-          <div className='max-w-[1400px] min-h-screen mx-auto '>
-            <RouterProvider router={router} />
+      <AuthProvider>
+        <CustomThemeProvider>
+          <div className='text-[#0300303] bg-[#F5F5F5]'>
+            <div className='max-w-[1400px] min-h-screen mx-auto '>
+              <RouterProvider router={router} />
+            </div>
           </div>
-        </div>
-      </CustomThemeProvider>
+        </CustomThemeProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/utility/AuthProvider/AuthProvider.jsx
+++ src/utility/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,58 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import Swal from "sweetalert2";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
 
-    const data = {}
+    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    // Create user with email and password
+    const createUserWithEmailPass = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const gmailUser = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    // Check User
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setLoading(false);
+            setUser(currentUser);
+        })
+        return () => unSubscribe();
+    }, [])
+
+    const logOut = () => {
+        return signOut(auth)
+            .then(res => {
+                if (res) {
+                    Swal.fire({
+                        title: "Log Out Success!",
+                        text: "See You Again",
+                        icon: "success"
+                    });
+                }
+            })
+            .catch(err => {
+                if (err) {
+                    Swal.fire({
+                        title: "Opps...",
+                        text: "Something is wrong!",
+                        icon: "error"
+                    });
+                }
+            })
+    }
+
+    const data = { createUserWithEmailPass, gmailUser, loading, user, logOut }
 
     return (
         <AuthContext.Provider value={data}>

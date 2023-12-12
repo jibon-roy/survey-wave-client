1c1329597542c1386ffbfbcf6601078e8b753cb9
jibon-roy <mailme.jibon@gmail.com>
2023-12-12 21:37:12 +0600

    user profile update functional adding

--- src/utility/AuthProvider/AuthProvider.jsx
+++ src/utility/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import Swal from "sweetalert2";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
@@ -85,8 +85,15 @@ const AuthProvider = ({ children }) => {
         return () => unSubscribe();
     }, [axiosPublic])
 
+    const updateUserProfileName = (name) => {
+        return updateProfile(user, { displayName: name })
+    }
+    const updateUserProfileImage = (photoUrl) => {
+        return updateProfile(user, { photoURL: photoUrl })
+    }
 
-    const data = { createUserWithEmailPass, logInWithEmailPass, gmailUser, loading, user, logOut }
+
+    const data = { createUserWithEmailPass, logInWithEmailPass, gmailUser, loading, user, logOut, updateUserProfileName, updateUserProfileImage }
 
     return (
         <AuthContext.Provider value={data}>

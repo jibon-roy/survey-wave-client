9dc6aa88f1c00bfc9641ce61f9c745b25ef2cbd0
jibon-roy <mailme.jibon@gmail.com>
2023-12-12 23:04:39 +0600

    photo change has updated

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -19,8 +19,9 @@ import Swal from "sweetalert2";
 
 const Dashboard = () => {
     let [isOpen, setIsOpen] = useState(false);
+    let [isOpen2, setIsOpen2] = useState(false);
     const axiosSecure = useAxiosSecure();
-    const { user, updateUserProfileName } = useAuthProvider()
+    const { user, updateUserProfileName, updateUserProfileImage } = useAuthProvider()
 
 
     function closeModal() {
@@ -29,6 +30,13 @@ const Dashboard = () => {
 
     function openModal() {
         setIsOpen(true)
+    }
+    function closeModal2() {
+        setIsOpen2(false)
+    }
+
+    function openModal2() {
+        setIsOpen2(true)
     }
 
     const { data } = useQuery({
@@ -53,7 +61,24 @@ const Dashboard = () => {
                 icon: "error"
             }))
     }
-    const userName = user?.displayName
+
+    const handleChangePhoto = (e) => {
+        e.preventDefault()
+        const photo = e.currentTarget.photo.value;
+        updateUserProfileImage(photo)
+            .then(() => Swal.fire({
+                title: "Successful",
+                text: `Photo has changed`,
+                icon: "success"
+            }).then(() => location.reload()))
+            .catch(() => Swal.fire({
+                title: "Opps!",
+                text: `Photo not changed`,
+                icon: "error"
+            }))
+    }
+
+    const userName = user?.displayName;
     const fullName = user?.displayName;
     const [role] = useRole()
     return (
@@ -154,7 +179,72 @@ const Dashboard = () => {
                                 </Dialog>
                             </Transition>
 
-                            <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Update Photo</a>
+                            <button
+                                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
+                                type="button"
+                                onClick={openModal2}
+                            >
+                                Update Photo
+                            </button>
+                            <Transition appear show={isOpen2} as={Fragment}>
+                                <Dialog as="div" className="z-10" onClose={closeModal2}>
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0"
+                                        enterTo="opacity-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <div className="fixed inset-0 bg-black/25" />
+                                    </Transition.Child>
+
+                                    <div className="fixed inset-0 overflow-y-auto">
+                                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                            <Transition.Child
+                                                as={Fragment}
+                                                enter="ease-out duration-300"
+                                                enterFrom="opacity-0 scale-95"
+                                                enterTo="opacity-100 scale-100"
+                                                leave="ease-in duration-200"
+                                                leaveFrom="opacity-100 scale-100"
+                                                leaveTo="opacity-0 scale-95"
+                                            >
+                                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                                    <form onSubmit={handleChangePhoto}>
+                                                        <div className="mx-auto text-2xl font-semibold text-center">Admin Action</div>
+                                                        <div className="w-full h-px bg-primary-text mt-4 mb-4"></div>
+
+                                                        <div className="text-primary-text mt-4 mb-2 mr-2">Change photo URL</div>
+                                                        <div className="mt-2">Your Photo URL<span className="text-red-600">*</span></div>
+                                                        <div className="px-4 border-2 border-primary-main py-2 bg-white rounded-lg dark:bg-gray-800">
+                                                            {/* <label htmlFor="photo" className="sr-only">Your feedback</label> */}
+                                                            <input defaultValue={user?.photoURL} id="photo" name='photo' className="w-full outline-none px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder='Write your name...' required></input>
+                                                        </div>
+                                                        <div className="flex gap-3">
+                                                            <button
+                                                                type="submit"
+                                                                className="text-white w-1/2 bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded-lg mt-4 text-lg"
+                                                            >
+                                                                Done
+                                                            </button>
+                                                            <button
+                                                                onClick={closeModal2}
+                                                                type="reset"
+                                                                className="text-primary-text w-1/2 bg-primary-bg2 border-2 border-primary-main py-2 px-8 focus:outline-none hover:bg-blue-200 rounded-lg mt-4 text-lg"
+                                                            >
+                                                                Cancel
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                </Dialog.Panel>
+                                            </Transition.Child>
+                                        </div>
+                                    </div>
+                                </Dialog>
+                            </Transition>
+
                         </div>
                     </div>
                 </div>

0c5b0f1fb9512a1872ca0a83f0ff69b3c76b33ad
jibon-roy <mailme.jibon@gmail.com>
2023-12-12 22:51:26 +0600

    name changing functionality added

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -7,15 +7,29 @@ import useRole from "../../hooks/useRole";
 import MyBarChart from "./Charts/BarChart";
 
 
-// import ApexCharts from 'apexcharts'
+// import useAuthProvider from "../../../hooks/useAuthProvider";
+import { useState } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+// import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 
 
 
 const Dashboard = () => {
-
+    let [isOpen, setIsOpen] = useState(false);
     const axiosSecure = useAxiosSecure();
-    const { user } = useAuthProvider()
+    const { user, updateUserProfileName } = useAuthProvider()
 
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
 
     const { data } = useQuery({
         queryKey: ['users'],
@@ -24,7 +38,21 @@ const Dashboard = () => {
             return res.data
         }
     })
-
+    const handleChangeName = (e) => {
+        e.preventDefault()
+        const name = e.currentTarget.name.value;
+        updateUserProfileName(name)
+            .then(() => Swal.fire({
+                title: "Successful",
+                text: `Name has changed`,
+                icon: "success"
+            }).then(() => location.reload()))
+            .catch(() => Swal.fire({
+                title: "Opps!",
+                text: `Name not changed`,
+                icon: "error"
+            }))
+    }
     const userName = user?.displayName
     const fullName = user?.displayName;
     const [role] = useRole()
@@ -60,7 +88,72 @@ const Dashboard = () => {
                         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{fullName}</h5>
                         <span className="text-sm text-gray-500 dark:text-gray-400">{role}</span>
                         <div className="flex mt-4 md:mt-6">
-                            <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-main rounded-lg hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update name</a>
+                            <button
+                                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-primary-main rounded-lg hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                type="button"
+                                onClick={openModal}
+                            >
+                                Change Name
+                            </button>
+                            <Transition appear show={isOpen} as={Fragment}>
+                                <Dialog as="div" className="z-10" onClose={closeModal}>
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0"
+                                        enterTo="opacity-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <div className="fixed inset-0 bg-black/25" />
+                                    </Transition.Child>
+
+                                    <div className="fixed inset-0 overflow-y-auto">
+                                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                            <Transition.Child
+                                                as={Fragment}
+                                                enter="ease-out duration-300"
+                                                enterFrom="opacity-0 scale-95"
+                                                enterTo="opacity-100 scale-100"
+                                                leave="ease-in duration-200"
+                                                leaveFrom="opacity-100 scale-100"
+                                                leaveTo="opacity-0 scale-95"
+                                            >
+                                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                                    <form onSubmit={handleChangeName}>
+                                                        <div className="mx-auto text-2xl font-semibold text-center">Admin Action</div>
+                                                        <div className="w-full h-px bg-primary-text mt-4 mb-4"></div>
+
+                                                        <div className="text-primary-text mt-4 mb-2 mr-2">Change name:</div>
+                                                        <div className="mt-2">Your name<span className="text-red-600">*</span></div>
+                                                        <div className="px-4 border-2 border-primary-main py-2 bg-white rounded-lg dark:bg-gray-800">
+                                                            <label htmlFor="comment" className="sr-only">Your feedback</label>
+                                                            <input defaultValue={user?.displayName} id="name" name='name' className="w-full outline-none px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder='Write your name...' required></input>
+                                                        </div>
+                                                        <div className="flex gap-3">
+                                                            <button
+                                                                type="submit"
+                                                                className="text-white w-1/2 bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded-lg mt-4 text-lg"
+                                                            >
+                                                                Done
+                                                            </button>
+                                                            <button
+                                                                onClick={closeModal}
+                                                                type="reset"
+                                                                className="text-primary-text w-1/2 bg-primary-bg2 border-2 border-primary-main py-2 px-8 focus:outline-none hover:bg-blue-200 rounded-lg mt-4 text-lg"
+                                                            >
+                                                                Cancel
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                </Dialog.Panel>
+                                            </Transition.Child>
+                                        </div>
+                                    </div>
+                                </Dialog>
+                            </Transition>
+
                             <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Update Photo</a>
                         </div>
                     </div>
